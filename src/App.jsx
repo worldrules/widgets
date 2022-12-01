9e98ba89c6a9ac1d95c3e4e1e16d597d034418e3
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-12-01 18:47:23 -0300

    kek

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import { React, useState } from "react";
 import { Accordion } from "./components/Accordion";
 
 const items = [
-  { title: "What is React ?" },
-  { content: "React is frontend javascript framework" },
-  { title: "Why use React ?" },
-  { content: "React is a favorite JS library among engineers" },
-  { title: "How do you use React ?" },
-  { content: "You use React by creating components" },
+  {
+    title: "What is React ?",
+    content: "React is frontend javascript framework",
+  },
+  {
+    title: "Why use React ?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React ?",
+    content: "You use React by creating components",
+  },
 ];
 
 function App() {
   return (
     <div>
-      <Accordion></Accordion>
+      <Accordion items={items}></Accordion>
     </div>
   );
 }
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export const Accordion = () => {
-  return (
-    <div>Accordion</div>
-  )
-}
+export const Accordion = ({ items }) => {
+  const onTitleClick = (i) => {
+    console.log("Title Clicked", i);
+  };
+
+  const renderedItems = items.map((item, i) => {
+    return (
+      <React.Fragment key={item.title}>
+        {" "}
+        <div className="title active" onClick={() => onTitleClick(i)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
