dd2448ba75be98e6bd9c55bd71c7cc1a201386aa
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-12-01 16:51:23 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Accordion } from "./components/Accordion";
 const items = [
   { title: "What is React ?" },
   { content: "React is frontend javascript framework" },
+  { title: "Why use React ?" },
+  { content: "React is a favorite JS library among engineers" },
+  { title: "How do you use React ?" },
+  { content: "You use React by creating components" },
 ];
 
 function App() {
